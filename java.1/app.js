ce700791bd939791ca6fe8cefdf05bdc804d6e76
Goken177 <146647141+Samuel-vilela@users.noreply.github.com>
2023-12-04 22:27:03 -0300

    java-1

---
+++ java.1/app.js
@@ -0,0 +1,13 @@
+function jedi(){
+    document.getElementById("mensagem").innerText = "venha para o lado da luz" 
+    document.getElementById("frase"). innerText = "Guerras Não Fazem Ninguém Maior" 
+    document.getElementById("imagem").src="jedi.gif"
+    document.body.style="background: #607479" 
+} 
+
+function sith(){
+    document.getElementById("mensagem").innerText = "venha para o lado da sombrio"
+    document.getElementById("frase"). innerText = "A paz é uma mentira, só a emoção importa"
+    document.getElementById("imagem").src="sith.gif"
+} 
+
---
+++ java.2/app.js
@@ -0,0 +1,8 @@
+function album1(){
+    document.getElementById("imagem").src="50-cent.jpg"
+    document.getElementById("frase").innerText = text
+} 
+
+function album2(){
+    document.getElementById("imagem").src="eminem.jpg"
+} 
